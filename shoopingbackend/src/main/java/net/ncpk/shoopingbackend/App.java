8b9cbf0f95085c2d886dcadb8deaa7438efa2cdd
Neha Chaturvedi <nehachaturvedi.nc8@gmail.com>
2018-07-24 12:25:26 +0530

    My project setup is done

---
+++ shoopingbackend/src/main/java/net/ncpk/shoopingbackend/App.java
@@ -0,0 +1,13 @@
+package net.ncpk.shoopingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
